0ff0818fd9f603790bb2ffafbd3878e26b0e7235
vjz3qz <vjz3qz@virginia.edu>
2023-07-23 20:07:00 -0400

    removed material tailwind

--- client/src/index.js
+++ client/src/index.js
@@ -4,19 +4,11 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
 
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './styles/index.css';
-// import App from './App';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
