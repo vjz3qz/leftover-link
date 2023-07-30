906296602c3eeeccc14c574bb6d56280d45cce13
vjz3qz <vjz3qz@virginia.edu>
2023-07-30 18:06:37 -0400

    added inventory

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
-import { React, useState } from 'react';
-import Home from './pages/Home';
-import Mission from './pages/Mission';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import NavigationBar from './components/NavigationBar';
-import FoodTracker from './pages/FoodTracker';
+import React, { useState, useContext } from "react";
+import { UserContext } from './contexts/UserContext';
+import Home from "./pages/Home";
+import Mission from "./pages/Mission";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NavigationBar from "./components/NavigationBar";
+import FoodTracker from "./pages/FoodTracker";
 
 function App() {
   const [Login, setLogin] = useState(false);
-
+  const { restaurantInfo } = useContext(UserContext);
+  
   return (
     <div className="App">
       <NavigationBar />
       <Home />
       <Mission />
+
       <hr class="w-1/2 h-1 mx-auto my-4 bg-gray-300 border-0 rounded md:my-10 dark:bg-gray-700" />
-      {Login && <FoodTracker />}
+      {Login && (
+        <div>
+          <FoodTracker />
+          <hr className="w-1/2 h-1 mx-auto my-4 bg-gray-300 border-0 rounded md:my-10 dark:bg-gray-700" />
+        </div>
+      )}
       <About />
       <Contact />
     </div>
