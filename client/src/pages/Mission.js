c471bd7c79f4c5f4ccd876187a7eb16a58e8a34f
vjz3qz <vjz3qz@virginia.edu>
2023-08-17 01:09:17 -0400

    pushed map below when screen gets smaller

--- client/src/pages/Mission.js
+++ client/src/pages/Mission.js
@@ -1,21 +1,31 @@
 import React from "react";
 import Map from "../components/Map";
 
-
 function Mission() {
   return (
-    <section id="mission" className="p-8">
+    <section id="mission" className="p-4 lg:p-8">
       <div className="max-w-3xl mx-auto">
         <h1 className="text-3xl lg:text-4xl font-bold mb-4 text-center">We noticed a problem...</h1>
-        <div className="flex flex-row space-x-4 h-96">
-          <p className="text-lg leading-relaxed flex-1">
-            Many restaurants and dining halls have copious amounts of food waste and we wanted to address this.
-            Our main goal is to make a difference in the sustainability climate and find a way to connect these leftover vendors with homeless shelters.
-          </p>
-          <Map />
+
+        <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4">
+          
+          {/* Text Section */}
+          <div className="flex-1">
+            <p className="text-lg leading-relaxed">
+              Many restaurants and dining halls have copious amounts of food waste and we wanted to address this.
+              Our main goal is to make a difference in the sustainability climate and find a way to connect these leftover vendors with homeless shelters.
+            </p>
+          </div>
+          
+          {/* Map Section */}
+          <div className="w-full lg:w-1/2 h-96">
+            <Map />
+          </div>
+
         </div>
       </div>
     </section>
   );
 }
+
 export default Mission;
