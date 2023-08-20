f3485c371d1e2fbf969d0bff6e7a29ba9fec8dad
vjz3qz <vjz3qz@virginia.edu>
2023-08-20 02:45:49 -0400

    removing code

--- client/src/utils/AvailableRestaurants.js
+++
@@ -1,9 +0,0 @@
-const API_URL = 'http://localhost:1234/api/restaurants-with-food';
-
-async function fetchAvailableRestaurants() {
-  const response = await fetch(API_URL);
-  const data = await response.json();
-  return data;
-}
-
-export { fetchAvailableRestaurants };
